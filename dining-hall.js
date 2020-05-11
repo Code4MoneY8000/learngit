9a82474f073d2f3c58668d935aa072a645e38fea
Zhaoliqun <1440169549@qq.com>
2020-05-11 22:12:44 +0800

    单例模式

--- dining-hall.js
+++ dining-hall.js
@@ -22,57 +22,96 @@ function Staff(name,payment){
     this.name = name
     this.payment = payment
 }
-Staff.prototype.work = function(){
-
-}
 
 function Waiter(name,payment){
     Staff.call(this,name,payment)
 }
-Waiter.prototype = Object.create(Staff.prototype)
-Waiter.prototype.constructor = Waiter
-Waiter.prototype.serve = function(list){
-    if(Array.isArray(list)){
-        this.list = list
-    }else{
-        alert("上菜啦")
-    }
+
+Waiter.prototype.serve = function(foodorder){
+    console.log(Customerlist[CustomerNum]+"先生点了"+foodorder.name)
+    Tony.cooking(foodorder)
 }
+Waiter.prototype.up = function(foodorder){
+    console.log(Customerlist[CustomerNum]+"先生的"+foodorder.name+"请享用")
+    Customerlist[CustomerNum].eat()
+}
+
+var SingleWaiter = (function(){
+    var waiter
+    return function(name,payment){
+        if(!waiter){
+            waiter = new Waiter(name,payment)
+        }
+        return waiter
+    }
+})()
 
 function Cook(name,payment){
     Staff.call(this,name,payment)
 }
-Cook.prototype = Object.create(Staff.prototype)
-Cook.prototype.constructor = Cook
-Cook.prototype.cooking = function(){
-    alert("菜做好了！")
+
+Cook.prototype.cooking = function(foodorder){
+    console.log(foodorder.name+"可以上了")
+   Jay.up(foodorder)
 }
 
+var SingleCook = (function(){
+    var cook
+    return function(name,payment){
+        if(!cook){
+            cook = new Cook(name,payment)
+        }
+        return cook
+    }
+})()
+var CustomerNum = 0
 function Customer(){
 
 }
+function foodNum(){
+    return parseInt(Math.floor(Math.random()*10))
+}
+var foodorder = {}
 Customer.prototype.order = function(){
-    alert("点菜")
+    var foodnum = foodNum()
+    while(foodNum>3){
+        foodnum = foodNum()
+    }
+    foodorder = foodList[foodnum]
+    Jay.serve(foodorder)
 }
 Customer.prototype.eat = function(){
-    alert("吃菜")
+    console.log(Customerlist[CustomerNum]+"走了")
+    CustomerNum++
+    Customerlist[CustomerNum] = new Customer()
 }
-function Food(name,cost,price){
-    this.name = name
-    this.cost = cost
-    this.price = price
-}
+var Customerlist =["no1","no2","no3","no4"]
+var foodList = [
+    {
+        name:"宫保鸡丁",
+        price:20},
+    {
+        name:"炒茄子",
+        price:15
+    },{
+        name:"红烧肉",
+        price:30
+    },{
+        name:"土豆炖牛腩",
+        price:45
+    }]
 
 var ifeRestaurant = new Restaurant({
     cash: 1000000,
-    seats: 20,
+    seats: 1,
     staff: []
 });
+//创建厨师和服务员
+var Jay = new SingleWaiter("Jay",20000);
+var Tony = new SingleCook("Tony", 30000);
+Customerlist[CustomerNum] = new Customer();
+ifeRestaurant.hire(Jay)
+ifeRestaurant.hire(Tony)
 
-var newCook = new Cook("Tony", 10000);
-ifeRestaurant.hire(newCook);
-
-console.log(ifeRestaurant.staff);
-
-ifeRestaurant.fire(newCook);
-console.log(ifeRestaurant.staff);
+//执行程序
+Customerlist[CustomerNum].order()
